6ed37493f43bef00b2545bd15796bc33b97f009e
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-03-04 16:12:54 +0700

    add line spacing test

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -4,20 +4,28 @@
 
 test_that("thaipdf_config_set() works", {
 
+  # Set New
   new_font <- "Sarabun"
-  new_config_ls <- thaipdf_config_set(thai_font = new_font)
+  line_spacing <- 2
 
-  expect_equal(new_config_ls[["thai_font"]], new_font)
+  new_config_ls <- thaipdf_config_set(thai_font = new_font, line_spacing = line_spacing)
 
+  expect_equal(new_config_ls[["thai_font"]], new_font) # Check font
+  expect_equal(new_config_ls[["line_spacing"]], line_spacing) # Check Spacing
+
+  # Reset
   default_config_ls <- thaipdf_config_set()
 
   expect_equal(default_config_ls[["thai_font"]], "TH Sarabun New")
+  expect_equal(default_config_ls[["line_spacing"]], 1.5)
 
 })
 
 # Config: Get -------------------------------------------------------------
 
 test_that("thaipdf_config_get() works",{
+
+
   config_ls <- thaipdf_config_get()
   # return list
   expect_type(config_ls, "list")
@@ -32,8 +40,12 @@ test_that("thaipdf_config_get() works",{
 
 test_that("thaipdf_config_validate() works", {
 
+  # Thai font
   expect_error(thaipdf_config_validate(thai_font = 1))
   expect_error(thaipdf_config_validate(thai_font = c("")))
   expect_error(thaipdf_config_validate(thai_font = c("a", "b")))
+
+  expect_error(thaipdf_config_validate(line_spacing = "a"))
+  expect_error(thaipdf_config_validate(line_spacing = 1:2))
 
 })
