60b42a315d1676041904f1784cae23d024d8d460
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-03-20 16:28:45 +0700

    test use_thai_preamble() use temp file

--- tests/testthat/test-use_thai_preamble.R
+++ tests/testthat/test-use_thai_preamble.R
@@ -3,15 +3,47 @@
 # Test: use_thai_preamble() -----------------------------------------------
 
 
-test_that("use_thai_preamble() works", {
+# test_that("use_thai_preamble() works (test in package root)", {
+#
+#   # create file
+#   path <- suppressMessages(use_thai_preamble())
+#   # Check exist & path
+#   expect_true(fs::file_exists("thai-preamble.tex"))
+#   expect_type(path, "character")
+#   # clean up
+#   unlink("thai-preamble.tex")
+#
+# })
 
-  # create file
-  path <- suppressMessages(use_thai_preamble())
-  # Check exist & path
+test_that("use_thai_preamble() generate file in working dir (use temp file)", {
+  # Set Working Dir to Temp Dir
+  .old_wd <- setwd(tempdir())
+
+  # use_thai_preamble
+  path <- suppressMessages(thaipdf::use_thai_preamble("thai-preamble.tex"))
+
   expect_true(fs::file_exists("thai-preamble.tex"))
-  expect_type(path, "character")
-  # clean up
-  fs::file_delete("thai-preamble.tex")
+  expect_true(fs::file_exists(path))
+  #fs::file_show("thai-preamble.tex")
+  # Clean up
+  unlink("thai-preamble.tex")
+  setwd(.old_wd)
 
 })
+
+
+test_that("use_thai_preamble() generate file in absolute location (use temp file)", {
+  # Create Temp file
+  tmp_file <- file.path(tempdir(), "thai-preamble.tex")
+  # use_thai_preamble
+  path <- suppressMessages(thaipdf::use_thai_preamble(tmp_file))
+
+  # Check file exist and output path works ?
+  expect_true(fs::file_exists(tmp_file))
+  expect_true(fs::file_exists(path))
+  # fs::file_show(tempdir())
+  # Clean up
+  unlink(tmp_file)
+})
+
 
