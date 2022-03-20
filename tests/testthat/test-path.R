bf2c1e988a835697fd032c831af079d9de770946
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-03-20 23:29:53 +0700

    global config & preamble not needed

--- tests/testthat/test-path.R
+++ tests/testthat/test-path.R
@@ -8,13 +8,8 @@ test_that("path() is working",{
 })
 
 
-test_that("thai_preamble() is working", {
-  path_pre <- thai_preamble()
-  expect_type(path_pre, "character")
-})
-
-
 test_that("before_body() is working", {
   path_bb <- before_body()
   expect_type(path_bb, "character")
+  expect_true(fs::file_exists(path_bb))
 })
